b263aeb0759753de0d8fea758135d6f6d4efe9c3
535amar <amarjithrh535@gmail.com>
2025-08-17 21:41:46 +0530

    Release v1.1.0: added Error handling, changed the name of the script

---
+++ run_fetchii.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# run_neofetch.sh (portable version)
+
+set -euo pipefail
+LOGFILE="./run_neofetch.log"
+
+echo "==== Running Neofetch ===" tee -a "$LOGFILE"
+
+if ! command -v git >/dev/null 2>&1; then
+   echo "Error: git is not installed. Please install git first." | tee -a "$LOGFILE"
+   exit 1
+fi
+
+if [ -f "./neofetch/neofetch" ]; then
+   echo "Cloning Neofetch repository..." tee -a "$LOGFILE"
+   if git clone --depth=1 https://github.com/535amar/System-info-script.git >>"$LOGFILE" 2>&1; then
+       echo "Clone successful." | tee -a "$LOGFILE"
+   else
+       echo "Error:Failed to clone Neofetch repo." | tee -a "$LOGFILE"
+       exit
+    fi
+fi
+
+if [ -x "./neofetch/neofetch" ]; then
+      ./neofetch/neofetch | tee -a "$LOGFILE"
+else
+   echo "Error: neofetch script npt found or not ececutable." | tee -a "$LOGFILE"
+   exit
+fi
