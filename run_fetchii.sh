a00a777a0dcae1df820410552386fe78d260a8f5
535amar <amarjithrh535@gmail.com>
2025-08-17 23:22:22 +0530

    Release v1.1.0: updated kit for fetchi

--- run_fetchii.sh
+++ run_fetchii.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
-# run_neofetch.sh (portable version)
+# run_neofetch.sh (portable version with logging)
 
 set -euo pipefail
-LOGFILE="./run_neofetch.log"
+LOGFILE="./run_fetchii.log"
 
-echo "==== Running Neofetch ===" tee -a "$LOGFILE"
+echo "==== Running Neofetch ===" | tee -a "$LOGFILE"
 
+# Check git
 if ! command -v git >/dev/null 2>&1; then
    echo "Error: git is not installed. Please install git first." | tee -a "$LOGFILE"
    exit 1
 fi
 
-if [ -f "./neofetch/neofetch" ]; then
-   echo "Cloning Neofetch repository..." tee -a "$LOGFILE"
+# Cloning
+if [ ! -d "./neofetch" ]; then
+   echo "Cloning Neofetch repository..." | tee -a "$LOGFILE"
    if git clone --depth=1 https://github.com/dylanaraps/neofetch.git >>"$LOGFILE" 2>&1; then
        echo "Clone successful." | tee -a "$LOGFILE"
    else
        echo "Error:Failed to clone Neofetch repo." | tee -a "$LOGFILE"
        exit 1
     fi
+else
+   echo "Neofetch repo already exists. Skipping clone." | tee -a "$LOGFILE"
 fi
 
+# Run neofetch  if executable
 if [ -x "./neofetch/neofetch" ]; then
       ./neofetch/neofetch | tee -a "$LOGFILE"
 else
